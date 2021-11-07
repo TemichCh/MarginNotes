c5bac337d4fd808a5edcb4e2077b32e580a05925
TemichCh <temich.42.ch@gmail.com>
2021-11-07 22:52:10 +0700

    Notes image move to String and add converters for Date datatype to store in room db

--- app/src/main/java/com/example/notesdemo/model/Notes.kt
+++ app/src/main/java/com/example/notesdemo/model/Notes.kt
@@ -3,7 +3,7 @@ package com.example.notesdemo.model
 import android.media.Image
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.sql.Date
+import java.util.*
 
 @Entity(tableName = "Notes")
 data class Notes(
@@ -12,7 +12,7 @@ data class Notes(
     val userId: String?=null, //на будущее для разделения по юзерам
     val noteName:String,
     val noteText:String,
-    val image: Image, //TODO: media.Image может быть заменить на путь к файлу локально
-    val createDate:Date,
-    val modifiedDate:Date
+    val image: String?=null, //TODO: media.Image может быть заменить на путь к файлу локально
+    val createDate: Date,
+    val modifiedDate:Date? = null
 )
