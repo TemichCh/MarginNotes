d0815f8e804e9e4d5ecc0c9c60ca429024beb49a
TemichCh <temich.42.ch@gmail.com>
2021-11-08 20:09:14 +0700

    make Notes Parcelable

--- app/src/main/java/com/example/notesdemo/model/Notes.kt
+++ app/src/main/java/com/example/notesdemo/model/Notes.kt
@@ -1,18 +1,54 @@
 package com.example.notesdemo.model
 
 import android.media.Image
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.notesdemo.DAO.Converters
 import java.util.*
 
 @Entity(tableName = "Notes")
 data class Notes(
     @PrimaryKey(autoGenerate = true)
     var noteId: Int? = null,
-    val userId: String?=null, //на будущее для разделения по юзерам
-    val noteName:String,
-    val noteText:String,
-    val image: String?=null, //TODO: media.Image может быть заменить на путь к файлу локально
+    val userId: String? = null, //на будущее для разделения по юзерам
+    val noteName: String,
+    val noteText: String,
+    val image: String? = null, //TODO: media.Image может быть заменить на путь к файлу локально
     val createDate: Date,
-    val modifiedDate:Date? = null
-)
+    var modifiedDate: Date? = null
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readString()?:"",
+        parcel.readString()?:"",
+        parcel.readString(),
+        Date(parcel.readLong()),
+        Date(parcel.readLong())
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(noteId)
+        parcel.writeString(userId)
+        parcel.writeString(noteName)
+        parcel.writeString(noteText)
+        parcel.writeString(image)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Notes> {
+        override fun createFromParcel(parcel: Parcel): Notes {
+            return Notes(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Notes?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
