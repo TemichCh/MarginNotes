a832eab0dd44e9fa2cb36a2555a16b135d8fcf6c
TemichCh <temich.42.ch@gmail.com>
2021-11-14 14:45:03 +0700

    notes field move to var

--- app/src/main/java/com/example/notesdemo/model/Notes.kt
+++ app/src/main/java/com/example/notesdemo/model/Notes.kt
@@ -1,11 +1,8 @@
 package com.example.notesdemo.model
 
-import android.media.Image
-import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.example.notesdemo.DAO.Converters
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 
@@ -15,42 +12,9 @@ data class Notes(
     @PrimaryKey(autoGenerate = true)
     var noteId: Int? = null,
     val userId: String? = null, //на будущее для разделения по юзерам
-    val noteName: String,
-    val noteText: String,
+    var noteName: String,
+    var noteText: String,
     var image: String? = null, //TODO: media.Image может быть заменить на путь к файлу локально
-    val createDate: Date,
+    var createDate: Date,
     var modifiedDate: Date? = null
-) : Parcelable /*{
-    constructor(parcel: Parcel) : this(
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-        parcel.readString()?:"",
-        parcel.readString()?:"",
-        parcel.readString(),
-        Date(parcel.readLong()),
-        Date(parcel.readLong())
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeValue(noteId)
-        parcel.writeString(userId)
-        parcel.writeString(noteName)
-        parcel.writeString(noteText)
-        parcel.writeString(image)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Notes> {
-        override fun createFromParcel(parcel: Parcel): Notes {
-            return Notes(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Notes?> {
-            return arrayOfNulls(size)
-        }
-    }
-}*/
+) : Parcelable
