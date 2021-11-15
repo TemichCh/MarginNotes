1971d91eb5852cbe728522213ba6769f153dc05f
TemichCh <temich.42.ch@gmail.com>
2021-11-15 23:44:54 +0700

    cleanup NotesRepository.kt

--- app/src/main/java/com/example/notesdemo/DAO/NotesRepository.kt
+++ app/src/main/java/com/example/notesdemo/DAO/NotesRepository.kt
@@ -1,13 +1,10 @@
 package com.example.notesdemo.DAO
 
-import android.content.Context
 import androidx.annotation.WorkerThread
 import com.example.notesdemo.model.Notes
 import kotlinx.coroutines.flow.Flow
 
 class NotesRepository(private val db: NotesDao) {
-
-    //var db:NotesDao = NotesLocalDb.getInstance(context)?.LocalNotesDao()!!
 
     val allNotes:Flow<MutableList<Notes>> = db.gelAllNotes()
 
