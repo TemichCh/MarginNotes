7943aea5482922d5a7feb2b051b14e0c64677049
TemichCh <temich.42.ch@gmail.com>
2021-11-16 12:25:00 +0700

    move isSelected -> selected and make @Ignore

--- app/src/main/java/com/example/notesdemo/model/Notes.kt
+++ app/src/main/java/com/example/notesdemo/model/Notes.kt
@@ -2,20 +2,24 @@ package com.example.notesdemo.model
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 
-@Entity(tableName = "Notes",ignoredColumns = ["isSelected"])
+
+@Entity(tableName = "Notes")
 @Parcelize
 data class Notes(
-    @PrimaryKey(autoGenerate = true)
-    var noteId: Int? = null,
-    val userId: String? = null, //на будущее для разделения по юзерам
+    @PrimaryKey(autoGenerate = true) var noteId: Int? = 0,
+    var userId: String? = null, //на будущее для разделения по юзерам
     var noteName: String,
     var noteText: String,
     var image: String? = null, //TODO: media.Image может быть заменить на путь к файлу локально
     var createDate: Date,
     var modifiedDate: Date? = null,
-    var isSelected:Boolean = false
-) : Parcelable
+
+    @Ignore var selected:Boolean = false
+) : Parcelable {
+    constructor () : this(0,null,"","",null,Date(),null)
+}
