3f8e33ef1cb5e061c52ad7adab035d97adbf80ba
TemichCh <temich.42.ch@gmail.com>
2021-11-16 23:20:06 +0700

    multiple delete in action

--- app/src/main/java/com/example/notesdemo/MainActivity.kt
+++ app/src/main/java/com/example/notesdemo/MainActivity.kt
@@ -20,12 +20,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var deleteItem: MenuItem
     private lateinit var searchItem: MenuItem
 
+    private var selectionModeEnabled = false
+    val adapter = NotesListAdapter()
+
     val notesVModel: NotesViewModel by viewModels {
         NotesViewModelFactory((application as NotesApplication).repository)
     }
 
-    private var selectionModeEnabled = false
-    val adapter = NotesListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -33,7 +34,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(main_activity_toolbar)
-
 
         recyclerview_notes.layoutManager = LinearLayoutManager(this)
         recyclerview_notes.adapter = adapter
@@ -54,11 +54,9 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        //allNotes
         notesVModel.allNotes.observe(this) { notes ->
             notes.let { adapter.setNotes(it) }
         }
-        //notesVModel.getNotesData().observe(this, {adapter.setNotes(it)})
 
         val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
 
@@ -67,22 +65,18 @@ class MainActivity : AppCompatActivity() {
             addItemDecoration(divider)
         }
 
-
         val fab = findViewById<FloatingActionButton>(R.id.fab_main_add_note)
         fab.setOnClickListener {
             val intent = Intent(this@MainActivity, EditNote::class.java)
             startActivity(intent)
         }
-
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_search, menu)
         deleteItem = menu.findItem(R.id.action_delete)
-        //val deleteView = deleteItem?.actionView as androidx.appcompat.widget.AppCompatTextView
         searchItem = menu.findItem(R.id.action_search)
-        val searchView = searchItem?.actionView as androidx.appcompat.widget.SearchView
+        val searchView = searchItem.actionView as androidx.appcompat.widget.SearchView
         searchView.queryHint = "Введите наименование для поиска"
         searchView.setOnQueryTextListener(object :
             androidx.appcompat.widget.SearchView.OnQueryTextListener {
@@ -96,19 +90,16 @@ class MainActivity : AppCompatActivity() {
                 notesVModel.handleSearchQuery(newText)
                 return true
             }
-
-
         })
-        return true//super.onCreateOptionsMenu(menu)
+        return true
     }
-
 
     private fun startSelection(note: Notes) {
         note.selected = !note.selected
         val index = adapter.notesList.indexOf(note)
-        adapter.notifyItemChanged(index);
-        if (adapter.notesList.none { note ->
-                note.selected
+        adapter.notifyItemChanged(index)
+        if (adapter.notesList.none { noteSel ->
+                noteSel.selected
             }) {
             setDefaultToolbar()
             selectionModeEnabled = false
@@ -131,7 +122,7 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
-    private fun deselectAllProducts() {
+    private fun deselectAllNotes() {
         adapter.notesList.forEachIndexed { position, note ->
             if (note.selected) {
                 val index = adapter.notesList.indexOf(note)
@@ -143,17 +134,42 @@ class MainActivity : AppCompatActivity() {
         selectionModeEnabled = false
     }
 
+    private fun deleteSelectedNotes() {
+        val selList = adapter.getSelectedList()
+        if (selList.isNotEmpty()) {
+            val firstSelItem = selList[0]
+            val position = adapter.notesList.indexOf(firstSelItem)
+            deleteNote(firstSelItem, position)
+        }
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
         android.R.id.home -> {
-            deselectAllProducts()
+            deselectAllNotes()
             true
         }
         R.id.action_delete -> {
-            //TODO
+            deleteSelectedNotes()
             true
         }
         else ->
             super.onOptionsItemSelected(item)
     }
 
+    private fun deleteNote(note: Notes, position: Int) {
+        val isNoteDeleted = notesVModel.delete(note)
+        isNoteDeleted.also { /*dataOrException ->
+            if (isProductDeleted != null) {*/
+            adapter.notesList.remove(note)
+            adapter.notifyItemRemoved(position)
+            adapter.notifyItemRangeChanged(position, adapter.notesList.size)
+            if (adapter.getSelectedList().isEmpty()) {
+                setDefaultToolbar()
+                selectionModeEnabled = false
+            }
+            deleteSelectedNotes()
+            //}
+
+        }
+    }
 }
