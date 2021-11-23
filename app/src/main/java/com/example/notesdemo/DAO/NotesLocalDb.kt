e13e43fd176bcb41410299abffe727c72b6a4462
TemichCh <temich.42.ch@gmail.com>
2021-11-24 00:35:58 +0700

    add welcome initial db record

--- app/src/main/java/com/example/notesdemo/DAO/NotesLocalDb.kt
+++ app/src/main/java/com/example/notesdemo/DAO/NotesLocalDb.kt
@@ -8,6 +8,10 @@ import androidx.room.TypeConverters
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.notesdemo.model.Notes
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.util.*
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 
 @Database(entities = [Notes::class], version = 1)
@@ -17,7 +21,7 @@ abstract class NotesLocalDb : RoomDatabase() {
     abstract fun LocalNotesDao(): NotesDao
 
     companion object {
-        private var INSTANCE: NotesLocalDb? = null
+        var INSTANCE: NotesLocalDb? = null
 
         fun getInstance(context: Context, scope: CoroutineScope): NotesLocalDb {
             return INSTANCE ?: synchronized(this) {
@@ -43,5 +47,22 @@ abstract class NotesLocalDb : RoomDatabase() {
 private class NotesItemsCallback(val scope: CoroutineScope) : RoomDatabase.Callback() {
     override fun onCreate(db: SupportSQLiteDatabase) {
         super.onCreate(db)
+        val initialNote = Notes(noteName = "Добро пожаловать!",noteText = """
+            Перед Вами простое приложение для создания заметок.
+            Это могут быть различные записи которые Вы хотели бы сохранить в Вашем телефоне.
+            Записи могут содержать "Заголовок", "Текст" и "Изображение" из Вашей галереи.
+            К сожаления т.к. это пока первая версия программы данные могут храниться только локально, на Вашем телефоне.
+            Если приложение получит поддержку и будет востребованно оно обзаведётся различными возможностями по хранению данных 
+            и их переносу в сеть между Вашими устройствами.
+            Желаю приятной работы.
+        """.trimIndent(), null ,Date())
+        Executors.newSingleThreadExecutor().execute {
+            NotesLocalDb.INSTANCE?.let {
+                scope.launch {
+                    it.LocalNotesDao().InsertNote(initialNote)
+                }
+            }
+        }
+
     }
 }
