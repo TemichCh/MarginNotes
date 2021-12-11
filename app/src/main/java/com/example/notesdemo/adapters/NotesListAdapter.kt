9059789616b5c4d7c9b41444d79a7b768624a089
TemichCh <temich.42.ch@gmail.com>
2021-12-11 23:07:09 +0700

    change forecast on dates, add type to views in NotesViewHolder

--- app/src/main/java/com/example/notesdemo/adapters/NotesListAdapter.kt
+++ app/src/main/java/com/example/notesdemo/adapters/NotesListAdapter.kt
@@ -5,6 +5,8 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -30,7 +32,7 @@ class NotesListAdapter : RecyclerView.Adapter<NotesViewHolder>() {
     //  1. закрыть поле приватностью - private
     //  2. сделать поле неизменяемым val - нам не надо менять ссылку на объект, мы будем просто
     //  менять содержимое списка, а не указывать другой список
-    var notesList = mutableListOf<Notes>()
+    val notesList = mutableListOf<Notes>()
 
     private var itemClickListener: ((Notes) -> Unit)? = null
 
@@ -41,7 +43,9 @@ class NotesListAdapter : RecyclerView.Adapter<NotesViewHolder>() {
         // FIXME вместо использования списка который нам дали, лучше чистить текущий наш список и
         //  добавлять все элементы из списка переданного в функцию в наш список - тогда не будет
         //  вероятности что передан как аргумент MutableList и после вызова setItems где-то он изменяется
-        this.notesList = notes.toMutableList()
+        notesList.clear()
+        notesList.addAll(notes)
+        //= notes.toMutableList()
         // FIXME в пару к notifyDataSetChanged стоит указать в init блоке адаптера setHasStableIds(true)
         //  чтобы были анимации изменений из коробки
         notifyDataSetChanged()
@@ -59,14 +63,16 @@ class NotesListAdapter : RecyclerView.Adapter<NotesViewHolder>() {
     override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
         val note = notesList[position]
         holder.itemName.text = note.noteName
-        holder.itemDate.text = if (note.modifiedDate != null)
+        holder.itemDate.text = (note.modifiedDate?:note.createDate).humanizeDiff(Date())
+
+            /*if (note.modifiedDate != null)
             // FIXME вместо форскаста лучше заиспользовать конструкцию
             //  (note.modifiedDate ?? note.createDate).humanizeDiff(Date())
             //  это будет более надежный вариант, защищенный от ошибок модификации кода
             //  (если в текущем коде изменить условие но не изменить тело - можем получить креш на форскасте)
             note.modifiedDate!!.humanizeDiff(Date())
         else
-            note.createDate.humanizeDiff(Date())
+            note.createDate.humanizeDiff(Date())*/
         holder.itemText.text = note.noteText
 
         // FIXME в биндинге ВСЕГДА надо обрабатывать обе ветки условий.
@@ -129,10 +135,10 @@ class NotesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     // FIXME для всех свойств надо указывать тип чтобы:
     //  1. при чтении было понятно что тут будет храниться
     //  2. IDE не ругалась что у нас тип от Java не понятно нуллабелен или нет
-    val itemDate = view.list_item_date
-    val itemName = view.list_item_name
-    val itemText = view.list_item_text
-    val image = view.iv_has_image
+    val itemDate: TextView = view.list_item_date
+    val itemName: TextView = view.list_item_name
+    val itemText: TextView = view.list_item_text
+    val image: ImageView = view.iv_has_image
 }
 
 
