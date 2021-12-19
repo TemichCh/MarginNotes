838777001010758ddf85846bf96b251741570d59
TemichCh <temich.42.ch@gmail.com>
2021-12-20 00:08:02 +0700

    add liveData for search and selectedNotes, comment Factory because its move to ViewModelFactory

--- app/src/main/java/com/example/notesdemo/viewmodel/NotesViewModel.kt
+++ app/src/main/java/com/example/notesdemo/viewmodel/NotesViewModel.kt
@@ -1,14 +1,30 @@
 package com.example.notesdemo.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.*
+import com.example.notesdemo.CreateOrEditNoteActivity
 import com.example.notesdemo.DAO.NotesRepository
 import com.example.notesdemo.model.Note
 import kotlinx.coroutines.launch
 
+
 class NotesViewModel(private val notesRep: NotesRepository) : ViewModel() {
 
-    val searchQuery =  MutableLiveData("")
-    val selectionMode: MutableLiveData<Boolean> = MutableLiveData(false)
+    /*
+    private val _openNoteEvent = MutableLiveData<EventListener>()
+
+    val openNoteEvent: LiveData<EventListener> = _openNoteEvent
+
+    private val channel = Channel<String>(Channel.BUFFERED)
+    val flow = channel.receiveAsFlow()
+*/
+    val searchQuery = MutableLiveData("")
+    val selectedNotes = MutableLiveData<MutableList<Int>>()
+
+    val selectionMode: MutableLiveData<Boolean> = Transformations.map(selectedNotes) {
+        it.isNotEmpty()
+    } as MutableLiveData<Boolean> //MutableLiveData(false)
+
 
     val allNotes: LiveData<List<Note>> =
         Transformations.switchMap(searchQuery) { searchText ->
@@ -19,37 +35,46 @@ class NotesViewModel(private val notesRep: NotesRepository) : ViewModel() {
             }
         }
 
-    fun insert(note: Note) = viewModelScope.launch {
-        notesRep.insertNote(note)
-    }
 
     fun delete(note: Note) = viewModelScope.launch {
         notesRep.deleteNote(note)
     }
 
-    fun update(note: Note) = viewModelScope.launch {
-        notesRep.updateNote(note)
+
+    fun onNotePressed(context: Context, noteId: Int) {
+        if (selectionMode.value != true) {
+            context.startActivity(CreateOrEditNoteActivity.createIntent(context, noteId))
+        } else {
+            if (selectedNotes.value?.contains(noteId) == true) {
+                selectedNotes.value?.remove(noteId)
+            } else {
+                selectedNotes.value?.add(noteId)
+            }
+        }
     }
 
-    //fun onNotePressed (listener:Liste)
+    fun onNoteLongClick(context: Context, noteId: Int) {
+        selectionMode.value = true
+        onNotePressed(context, noteId)
+    }
 
-    // FIXME не нужен сам метод, как выше в комменте к лайвдате сказано. и также не нужно делать
-    //  изменение значения лайвдаты внутри скоупа - корутина тут не нужна
-    /*fun handleSearchQuery(text: String) = viewModelScope.launch {
-        searchQuery.value = text
-    }*/
+    fun deselectAllItems() {
+        selectedNotes.value?.clear()
+        selectionMode.value = false
+    }
+
 }
 
 // FIXME вместо фиксированной фабрики можно сделать класс общего назначения (с Generic), который
 //  принимает лямбду создающую нужный тип вьюмодели и тогда использование будет
 //  LambdaViewModelFactory { NotesViewModel(notesRepo) }
 //  и из этого файла данную фабрику надо будет вынести
-class NotesViewModelFactory(private val notesRep: NotesRepository) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(NotesViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return NotesViewModel(notesRep) as T
-        }
-        throw IllegalArgumentException("Unknown VieModel Class")
-    }
-}
+//class NotesViewModelFactory(private val notesRep: NotesRepository) : ViewModelProvider.Factory {
+//    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+//        if (modelClass.isAssignableFrom(NotesViewModel::class.java)) {
+//            @Suppress("UNCHECKED_CAST")
+//            return NotesViewModel(notesRep) as T
+//        }
+//        throw IllegalArgumentException("Unknown VieModel Class")
+//    }
+//}
