25b0a73034e809d21dd6ea1b7ae0b02255b9c990
TemichCh <temich.42.ch@gmail.com>
2021-12-25 11:08:55 +0700

    move class to object Converters.kt

--- app/src/main/java/com/example/notesdemo/DAO/Converters.kt
+++ app/src/main/java/com/example/notesdemo/DAO/Converters.kt
@@ -3,9 +3,9 @@ package com.example.notesdemo.dao
 import androidx.room.TypeConverter
 import java.util.*
 
-// FIXME это должен быть object - нам нет необходимости создавать объекты типа Converters
+// это должен быть object - нам нет необходимости создавать объекты типа Converters
 //  https://kotlinlang.org/docs/object-declarations.html#object-declarations-overview
-class Converters {
+object Converters {
     @TypeConverter
     fun fromTimestamp(value: Long?): Date? {
         return value?.let { Date(it) }
