d0833e600c0f6d40409d7e00d18218b0380e057a
TemichCh <temich.42.ch@gmail.com>
2022-01-08 12:14:05 +0700

    fix WildcardImport

--- app/src/main/java/com/example/notesdemo/MainActivity.kt
+++ app/src/main/java/com/example/notesdemo/MainActivity.kt
@@ -5,7 +5,10 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.selection.*
+import androidx.recyclerview.selection.SelectionPredicates
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StableIdKeyProvider
+import androidx.recyclerview.selection.StorageStrategy
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notesdemo.adapters.NotesListAdapter
